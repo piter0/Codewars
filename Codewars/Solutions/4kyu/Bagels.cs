757dc4e9d41d7a799ac9378a833212fa96a9e415
piter0 <piter0@piter0>
2021-05-31 19:14:51 +0200

    Add Bagels task

---
+++ Codewars/Solutions/4kyu/Bagels.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace Codewars.Solutions._4kyu
+{
+    class Bagels
+    {
+        public static Bagel Bagel
+        {
+            get
+            {
+                var b = new Bagel();
+                PropertyInfo property = typeof(Bagel).GetProperty("Value");
+                property.DeclaringType.GetProperty("Value");
+                property.SetValue(b, 4, BindingFlags.NonPublic | BindingFlags.Instance, null, null, null);
+                return b;
+            }
+        }
+    }
+
+    sealed class Bagel
+    {
+        public int Value { get; private set; } = 3;
+    }
+}
